3b6cfceb5bec0910b037342bdf603b99cbb7bf84
LisanneW <lisannew98@gmail.com>
2021-05-28 14:13:39 +0200

    small fix for forward

--- src/ConstantPropagation.hs
+++ src/ConstantPropagation.hs
@@ -25,9 +25,7 @@ lambdaF :: M.Map Int Block -> Int -> M.Map String Ztop -> M.Map String Ztop
 lambdaF ib i m = transferFromBlock (M.findWithDefault (S (Skip' 0)) i ib) m
 
 transferFromBlock :: Block -> M.Map String Ztop -> M.Map String Ztop
-transferFromBlock (S (IAssign' l n v)) m = case (all (== Top) (M.elems m)) of
-                                           True  -> m
-                                           False -> M.insert n (analyseExpression v m) m                      
+transferFromBlock (S (IAssign' l n v)) m = M.insert n (analyseExpression v m) m                     
 transferFromBlock _                    m = m
 
 analyseExpression :: IExpr -> M.Map String Ztop -> Ztop
